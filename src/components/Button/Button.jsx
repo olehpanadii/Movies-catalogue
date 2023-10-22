bb5ee75c4c323d96b09f2066d5ba2e5e899836f4
OlegPanadiy <opanadiy@gmail.com>
2023-10-22 17:19:52 +0100

    file update

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonWrapper, LoadMoreButton } from './Button.styled';
-
-export const LoadMoreBtn = ({ action }) => {
-  return (
-    <ButtonWrapper>
-      <LoadMoreButton onClick={action}>Load more</LoadMoreButton>
-    </ButtonWrapper>
-  );
-};
